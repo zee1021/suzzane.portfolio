b5aa4f921a4d1b8b1c178ee98ffd4942ead7370e
zee1021 <46909436+zee1021@users.noreply.github.com>
2026-02-20 12:36:48 +0800

    Implement ProductController with sample products

---
+++ ProductController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        // Criteria: Implemented an array based on a chosen theme (Tech Gadgets)
+        $products = [
+            ['name' => 'MacBook Pro', 'category' => 'Laptop', 'price' => 120000, 'stock' => 5],
+            ['name' => 'iPhone 15', 'category' => 'Mobile', 'price' => 60000, 'stock' => 12],
+            ['name' => 'iPad Air', 'category' => 'Tablet', 'price' => 35000, 'stock' => 8],
+            ['name' => 'AirPods Pro', 'category' => 'Audio', 'price' => 15000, 'stock' => 20],
+            ['name' => 'Apple Watch', 'category' => 'Wearable', 'price' => 25000, 'stock' => 10],
+        ];
+
+        return view('products.index', compact('products'));
+    }
+}
